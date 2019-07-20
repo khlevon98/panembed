451a74019adf50b1f932b1a5b768282e3a327702
khlevon98 <kh.levon98@gmail.com>
2019-07-20 21:15:57 +0400

    project actions and reducer

--- src/store/actions/project.js
+++ src/store/actions/project.js
@@ -1,3 +1,11 @@
+import createAction from './create-action';
+import ApiService from '../../services/api-service';
+import ErrorService from '../../services/error-service';
+
+/**
+ * ACTION TYPES
+ */
+
 export const FETCH_PROJECTS_REQUEST = 'FETCH_PROJECTS_REQUEST';
 export const FETCH_PROJECTS_SUCCESS = 'FETCH_PROJECTS_SUCCESS';
 export const FETCH_PROJECTS_FAILURE = 'FETCH_PROJECTS_FAILURE';
@@ -6,6 +14,70 @@ export const FETCH_PROJECT_REQUEST = 'FETCH_PROJECT_REQUEST';
 export const FETCH_PROJECT_SUCCESS = 'FETCH_PROJECT_SUCCESS';
 export const FETCH_PROJECT_FAILURE = 'FETCH_PROJECT_FAILURE';
 
-export const DELETE_PROJECT_REQUEST = 'FETCH_PROJECT_REQUEST';
-export const DELETE_PROJECT_SUCCESS = 'FETCH_PROJECT_SUCCESS';
-export const DELETE_PROJECT_FAILURE = 'FETCH_PROJECT_FAILURE';
+export const CREATE_PROJECT_REQUEST = 'CREATE_PROJECT_REQUEST';
+export const CREATE_PROJECT_SUCCESS = 'CREATE_PROJECT_SUCCESS';
+export const CREATE_PROJECT_FAILURE = 'CREATE_PROJECT_FAILURE';
+
+export const DELETE_PROJECT_REQUEST = 'DELETE_PROJECT_REQUEST';
+export const DELETE_PROJECT_SUCCESS = 'DELETE_PROJECT_SUCCESS';
+export const DELETE_PROJECT_FAILURE = 'DELETE_PROJECT_FAILURE';
+
+/**
+ * THUNKS
+ */
+
+export const fetchProjects = query => (dispatch, getState, { getFirebase, getFirestore }) => {
+  const Api = new ApiService(getFirestore(), getFirebase());
+
+  dispatch(createAction(FETCH_PROJECTS_REQUEST));
+
+  Api.getProjects(query)
+    .then(payload => {
+      dispatch(createAction(FETCH_PROJECTS_SUCCESS, payload.data));
+    })
+    .catch(payload => {
+      dispatch(createAction(FETCH_PROJECTS_FAILURE, ErrorService.parse(payload)));
+    });
+};
+
+export const fetchProject = id => (dispatch, getState, { getFirebase, getFirestore }) => {
+  const Api = new ApiService(getFirestore(), getFirebase());
+
+  dispatch(createAction(FETCH_PROJECT_REQUEST));
+
+  Api.getProject(id)
+    .then(payload => {
+      dispatch(createAction(FETCH_PROJECT_SUCCESS, payload.data));
+    })
+    .catch(payload => {
+      dispatch(createAction(FETCH_PROJECT_FAILURE, ErrorService.parse(payload)));
+    });
+};
+
+export const createProject = data => (dispatch, getState, { getFirebase, getFirestore }) => {
+  const Api = new ApiService(getFirestore(), getFirebase());
+
+  dispatch(createAction(CREATE_PROJECT_REQUEST));
+
+  Api.createProject(data)
+    .then(payload => {
+      dispatch(createAction(CREATE_PROJECT_SUCCESS, payload.data));
+    })
+    .catch(payload => {
+      dispatch(createAction(CREATE_PROJECT_FAILURE, ErrorService.parse(payload)));
+    });
+};
+
+export const deleteProject = id => (dispatch, getState, { getFirebase, getFirestore }) => {
+  const Api = new ApiService(getFirestore(), getFirebase());
+
+  dispatch(createAction(DELETE_PROJECT_REQUEST));
+
+  Api.deleteProject(id)
+    .then(payload => {
+      dispatch(createAction(DELETE_PROJECT_SUCCESS, payload.data));
+    })
+    .catch(payload => {
+      dispatch(createAction(DELETE_PROJECT_FAILURE, ErrorService.parse(payload)));
+    });
+};
