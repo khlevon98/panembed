aeca2018122a391ca4d856f623d0487d88fc7bf3
khlevon98 <kh.levon98@gmail.com>
2019-07-20 15:57:50 +0400

    add navigation

--- src/layouts/main/header/header.js
+++ src/layouts/main/header/header.js
@@ -6,22 +6,26 @@ import './header.scss';
 import { Navbar } from 'react-materialize';
 import { NavLink } from 'react-router-dom';
 
+import { DefaultNavigation, AuthNavigation } from '../../../components/navigation';
+
 const AppHeader = () => {
   return (
     <header className="App-header">
       <Navbar
+        alignLinks="right"
+        className="teal"
         brand={
           <NavLink to="/" className="valign-wrapper d-flex min-h-100">
             <Logo className="d-block logo" />
           </NavLink>
         }
-        alignLinks="right"
-        className="teal"
       >
-        <NavLink to="/">Getting started</NavLink>
+        <>
+          <DefaultNavigation />
+          <AuthNavigation />
+        </>
       </Navbar>
     </header>
   );
 };
-
 export default AppHeader;
