df256cf1a8ef5dab6cde083f0946d3c7e7fbbeca
khlevon98 <kh.levon98@gmail.com>
2019-07-20 16:02:26 +0400

    add firebase reducers and separate auth store from user

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,11 +1,18 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
+import { firestoreReducer } from 'redux-firestore';
+import { firebaseReducer } from 'react-redux-firebase';
 
+import authReducer from './auth';
 import userReducer from './user';
 import projectReducer from './project';
 
 const rootReducer = history =>
   combineReducers({
+    firestore: firestoreReducer,
+    firebase: firebaseReducer,
+
+    auth: authReducer,
     user: userReducer,
     project: projectReducer,
     router: connectRouter(history),
