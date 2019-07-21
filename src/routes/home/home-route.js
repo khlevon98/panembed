436710a9032810c15ee01232cd6029b441bc64f8
khlevon98 <kh.levon98@gmail.com>
2019-07-21 15:43:29 +0400

    code refactor ... get data from store

--- src/routes/home/home-route.js
+++ src/routes/home/home-route.js
@@ -4,120 +4,62 @@ import { connect } from 'react-redux';
 import ErrorBoundary from '../../components/error-boundary';
 
 import ProjectList from '../../components/project-list';
-
-import sampleImg from '../../assets/images/sample.jpg';
-
-let idCounter = 0;
-
-const fackeData = [
-  {
-    id: ++idCounter,
-    title: 'I am a very simple card.',
-    image: sampleImg,
-    owner: {
-      id: 1,
-    },
-  },
-  {
-    id: ++idCounter,
-    title: 'I am a very simple card.',
-    image: sampleImg,
-    owner: {
-      id: 1,
-    },
-  },
-  {
-    id: ++idCounter,
-    title: 'I am a very simple card.',
-    image: sampleImg,
-    owner: {
-      id: 1,
-    },
-  },
-  {
-    id: ++idCounter,
-    title: 'I am a very simple card.',
-    image: sampleImg,
-    owner: {
-      id: 1,
-    },
-  },
-  {
-    id: ++idCounter,
-    title: 'I am a very simple card.',
-    image: sampleImg,
-    owner: {
-      id: 1,
-    },
-  },
-  {
-    id: ++idCounter,
-    title: 'I am a very simple card.',
-    image: sampleImg,
-    owner: {
-      id: 1,
-    },
-  },
-  {
-    id: ++idCounter,
-    title: 'I am a very simple card.',
-    image: sampleImg,
-    owner: {
-      id: 1,
-    },
-  },
-  {
-    id: ++idCounter,
-    title: 'I am a very simple card.',
-    image: sampleImg,
-    owner: {
-      id: 1,
-    },
-  },
-  {
-    id: ++idCounter,
-    title: 'I am a very simple card.',
-    image: sampleImg,
-    owner: {
-      id: 1,
-    },
-  },
-  {
-    id: ++idCounter,
-    title: 'I am a very simple card.',
-    image: sampleImg,
-    owner: {
-      id: 1,
-    },
-  },
-  {
-    id: ++idCounter,
-    title: 'I am a very simple card.',
-    image: sampleImg,
-    owner: {
-      id: 1,
-    },
-  },
-  {
-    id: ++idCounter,
-    title: 'I am a very simple card.',
-    image: sampleImg,
-    owner: {
-      id: 1,
-    },
-  },
-];
+import { fetchProjects } from '../../store/actions/project';
+import { compose } from 'redux';
+import { withRouter } from 'react-router';
+import Spinner from '../../components/spinner';
+import PropTypes from 'prop-types';
 
 class HomeRoute extends React.Component {
+  componentDidMount() {
+    this.props.getProjects();
+  }
+
   render() {
+    const {
+      projects: { data, isLoaded },
+    } = this.props;
+
+    if (!isLoaded) {
+      return <Spinner />;
+    }
+
     return (
       <section className="section">
         <ErrorBoundary>
-          <ProjectList data={fackeData} />
+          <ProjectList data={data} />
         </ErrorBoundary>
       </section>
     );
   }
 }
 
-export default connect()(HomeRoute);
+HomeRoute.propTypes = {
+  projects: PropTypes.shape({
+    isLoaded: PropTypes.bool,
+    data: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
+  getProjects: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    projects: state.project.list,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getProjects: () => dispatch(fetchProjects()),
+  };
+}
+
+const enhance = compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
+  withRouter
+);
+
+export default enhance(HomeRoute);
