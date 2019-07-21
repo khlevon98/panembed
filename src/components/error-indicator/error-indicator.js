fd36c2f0a7cee891ad3b6c92a4042a855b570cae
khlevon98 <kh.levon98@gmail.com>
2019-07-21 15:38:34 +0400

    ErrorIndicator component style changes

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -5,9 +5,8 @@ import './error-indicator.scss';
 const ErrorIndicator = () => {
   return (
     <div className="error-indicator">
-      <span className="boom">BOOM!</span>
-      <span>something has gone terribly wrong</span>
-      <span>(but we already sent droids to fix it)</span>
+      <h3 className="boom mb-0 red-text">BOOOOOOM!</h3>
+      <p>something has gone terribly wrong</p>
     </div>
   );
 };
